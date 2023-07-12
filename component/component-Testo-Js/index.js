0f1e5bc638dc1620b7dba883d9cc37c0b0aab2ba
Ruberto Marco <rubertomarco48@gmail.com>
2023-07-12 21:40:58 +0200

    feat:partial

--- component/component-Testo-Js/index.js
+++ component/component-Testo-Js/index.js
@@ -1,15 +1,25 @@
 const i = document.getElementsByClassName("objJavascript");
+const p = document.getElementsByClassName("testoJs");
 /* i.addEventListener("onmouseover",img); */
 function img() {
-
-    if (document.getElementById("img_new")) {
-        return;
-    }else{
-    let div = document.createElement("div");
-    div.id = "img_new";
-    div.style["background-color"]= "red";
-    div.style.height= "50px";
-    div.style.width="50px";
-    i[0].appendChild(div)
-    }
+  if (!document.getElementById("img_new")) {
+    i[0].style.zIndex = "20";
+    let img = document.createElement("img");
+    img.src = i[0].attributes.imghover.nodeValue;
+    img.id = "img_new";
+    img.style.width = "200px";
+    img.style.position = "absolute";
+    img.style.borderRadius = "1rem";
+    img.style.zIndex = "10";
+    i[0].appendChild(img);
+    i[0].addEventListener("mousemove", (e) => {
+      img.style.left = e.clientX -400  + "px";
+      img.style.top = e.clientY  -100 + "px";
+     
+    });
+  }
 }
+i[0].addEventListener("mouseenter", img);
+i[0].addEventListener("mouseout", () => {
+  document.getElementById("img_new").remove();
+});
